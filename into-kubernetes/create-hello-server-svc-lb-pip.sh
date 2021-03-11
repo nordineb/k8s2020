b2668c1fd94c883655a2f978727f4290a0dde6af
Nordine Ben Bachir <nordineb@gmail.com>
2021-03-11 21:35:12 +0100

    deleted all files

--- into-kubernetes/create-hello-server-svc-lb-pip.sh
+++
@@ -1,4 +0,0 @@
-kubectl expose service hello-server --name hello-server-svc-lb-pip --type LoadBalancer --port 80 --target-port 8080
-watch kubectl get svc
-## Check the new LB in Google Cloud Portal
-kubectl delete service hello-server-svc-lb-pip
